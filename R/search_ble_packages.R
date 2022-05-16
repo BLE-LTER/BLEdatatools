f772a53b5adcefd94aa9efe6c3512fd9ba558935
Emily Bristol <58533514+bristol-emily@users.noreply.github.com>
2022-05-16 17:48:09 -0500

    Removed publication dates from default parameter.

--- R/search_ble_packages.R
+++ R/search_ble_packages.R
@@ -1,15 +1,15 @@
 #' Search all BLE LTER packages
 #'
 #' Modify EDI utils search_data_packages function to return only BLE LTER data. Default is to provide a dataframe with
-#' all BLE package id ("scope.identifier.revision" format), title, publication dates, and the package number from
+#' all BLE package id ("scope.identifier.revision" format), title, and the package identifier from
 #' package id.
 #'
 #'
-#' @param fields (string) Searchable metadata fields in EDI repository to include query that produces output.
+#' @param fields (character) Searchable metadata fields in EDI repository to include query that produces output.
 #' The way this function is written now (badly), this string must follow the Solr query syntex (e.g. "id,packageid,title").
 #'
 #'
-#' @param env (string) Repository environment. Can be "production", "staging", or "development".
+#' @param env (character) Repository environment. Can be "production", "staging", or "development".
 #'
 #' @return
 #' @export
@@ -17,7 +17,7 @@
 #' @examples
 #'
 
-search_ble_packages <- function(fields = 'id,packageid,title,pubdate', env = 'production') {
+search_ble_packages <- function(fields = 'id,packageid,title', env = 'production') {
 
   scope <- 'knb-lter-ble'
 
