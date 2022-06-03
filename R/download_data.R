70854963c5a9f2b1af1667ff0f9fb0d5f818f603
Emily Bristol <58533514+bristol-emily@users.noreply.github.com>
2022-06-03 12:18:11 -0500

    Rename download_any_ble_data as download_data.

--- R/download_data.R
+++ R/download_data.R
@@ -1,84 +1,128 @@
+#' Download a BLE LTER data package or data entity from EDI
+#'
+#' @param identifier (numeric) Package identifier number that corresponds with
+#' the EDI package ID. Can only specify one identifier at a time.
+#' @param entity_number (numeric). Entity identifier number. Optional.
+#' Can only specify one entity at a time. Entity must be a .csv file or function
+#' will produce an error.
+#' @param path (character) Path to write files to. If left blank, will default
+#' to the working directory.
+#' @param write (boolean) Do you want to write the entity as a .csv file, or do
+#' you want to save it as an R object.
+#'
+#' @return Zip folder containing all package data entities, one .csv file
+#' containing specified data entity, or a data.frame containing the specified
+#' data entity.
+#'
+#' @examples # download the third entity in package 1 as a .csv
+#' download_data(identifier = 1, entity_number = 3)
+#'
+#' # download all entities in package 1 in a zip file
+#' download_data(identifier = 1)
+#'
+#' # save DOC/TDN data (package 2, entity 1) as an R object
+#' DOC <- download_data(identifier = 12, 1)
+#'
 
-
-#' Download BLE data files
-#'
-#' @param ids (numeric) Vector of dataset IDs to grab
-#' @param path (character) Path to write CSVs to
-#' @param write (boolean) Whether to write to file
-#'
-#' @return (list) A nested list of data frames. 1st-level list item corresponds to dataset, 2nd-level items correspond to entities within a dataset and contain a data.frame of the data entity. List items are named accordingly (dataset after the packageId and entities after the entity name). Optionally, if write=T, functions also write to CSVs files in specified path. File names are the full package Id, followed by two underscores, followed by the full entity name.
-#' @export
-#'
-#' @examples
-download_data <- function(ids = NULL,
+download_data <- function(identifier,
+                          entity_number = NULL,
                           path = NULL,
-                          write = F) {
+                          write = NULL) {
+
   if (is.null(path))
     path <- getwd()
 
   # checking if path exists
   stopifnot(dir.exists(path))
 
-  e <- decide_entities(ids)
+  # add an error message here if user does not supply a proper package id number
+  stopifnot(is.numeric(identifier))
 
-  message("Downloading data files...")
+  possible_ids <-
+    EDIutils::list_data_package_identifiers(scope = "knb-lter-ble")
 
+  # error message if any ID supplied is not in our list
 
-  # if you read this bit of code
-  # beware that all the "raw"s are different
-  # lexical scoping!
+  if (!identifier %in% possible_ids) {
+    stop(
+      paste0(identifier,
+      " is not a valid BLE LTER package identifier.
+      Use the search_ble_packages function to find the correct identifier. \n"
+      )
+    )
+  } else {
 
-  # and I dislike the IFs sequences I'm resorting to here
-  # would like more elegance
-  raw <- lapply(seq_along(e), function(x) {
-    if (length(e[[x]]) > 1) {
-      # not particularly proud of this sequence
-      raw <- lapply(seq_along(e[[x]]), function(y) {
-        raw <- EDIutils::read_data_entity(packageId = names(e)[x],
-                                   entityId = e[[x]][[y]])
-        return(raw)
-      })
+    # use our search_ble_packages function to find the name of the data package
+    ble_packages <- search_ble_packages()
+    title <-
+      ble_packages[[which(ble_packages$identifier == identifier), "title"]]
 
-    }
-    else {
-      raw <- list(EDIutils::read_data_entity(packageId = names(e)[x],
-                                 entityId = e[[x]]))
-    }
-    names(raw) <- names(e[[x]]) # attach entity names
-    return(raw)
-  })
-
-  # attach pkg ids
-  names(raw) <- names(e)
-
-  dfs <- invisible(rrapply::rrapply(
-    object = raw,
-    f = readr::read_csv,
-    how = "list",
-    show_col_types = F # argument to read_csv
-  ))
-
-  # writing to path is optional???
-  # doesn't work right now
-  if (!is.null(path) && write) {
-    message("Writing data to files...")
-
-    lapply(seq_along(dfs), function(x) {
-      ds <- dfs[[x]]
-      if (length(ds) > 1) {
-        lapply(seq_along(ds), function(y) {
-          readr::write_csv(x = ds[[y]],
-                    file = file.path(path, paste0(names(dfs)[[x]], "__", names(ds)[[y]], ".csv")))
-        })
-      } else {
-        readr::write_csv(x = ds[[1]],
-                  file = file.path(path, paste0(names(dfs)[[x]], "__", names(ds)[[1]], ".csv"))
-                )
-      }
-
-    })
+    # tell the user the name of the package they specified with the identifier
+    message(paste0("Found your package titled '", title, "'."))
   }
 
-  message("Done.")
-  return(dfs)
+  # find most recent version of package
+  rev <- max(EDIutils::list_data_package_revisions(scope = "knb-lter-ble",
+                                         identifier = identifier,
+                                         env = "production"))
+
+  # define the packageId in scope, identifier, revision format
+  packageId <- paste0("knb-lter-ble.", identifier, ".", rev)
+
+  # if entity number is not specified, download the whole package in a zip folder
+  if (is.null(entity_number)) {
+   transaction <- EDIutils::create_data_package_archive(packageId)
+   message(paste0("Downloading a zip file containing the newest version of the
+                  package: ", packageId, "."))
+   EDIutils::read_data_package_archive(packageId, transaction, path = path)
+  } else {
+
+    # read and save the entity ids and names in the package
+    entityNames <- EDIutils::read_data_entity_names(packageId = packageId)
+
+    # error function if the entity number does not exist
+    possible_entities <- seq(1, nrow(entityNames), by = 1)
+    if (!entity_number %in% possible_entities) {
+      stop(paste0(entity_number,
+      " is not a valid entity number for the package you specified.
+      Use EDIutils::read_data_entity_names('",
+      packageId,
+      "') to see the entities that exist."))
+    }
+
+    # pull out the specified entity id and name
+    entityId <- entityNames[[entity_number, "entityId"]]
+    entityName <- entityNames[[entity_number, "entityName"]]
+
+    # read the raw data file
+    raw <- EDIutils::read_data_entity(packageId = packageId,
+                                      entityId = entityId,
+                                      env = "production")
+
+    # convert the raw data to a dataframe
+    data <- readr::read_csv(file = raw, show_col_types = F)
+
+    if (write) {
+
+      # write a csv to the specified path or working directory
+      message(paste0("Writing the entity called '",
+                     entityName,
+                     "' from ",
+                     packageId, "."))
+      readr::write_csv(data,
+                       file = file.path(path,
+                                        paste0(packageId, "-entity",
+                                               entity_number, ".csv")))
+
+    } else {
+      # otherwise return the dataframe
+      message(paste0("Here is the entity called '",
+                     entityName,
+                     "'from ",
+                     packageId, "."))
+      return(data)
+
+    }
+  }
 }
+
