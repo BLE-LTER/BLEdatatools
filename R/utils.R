e8834b1e21b6e176f255235e484a4dd27ed859c7
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2023-06-16 12:54:54 -0500

    adding a number of utility functions

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,9 @@
 #' Get entity names and IDs from newest revisions of BLE data sets slated for collations
 #'
-#' @param ids (numeric) Vector of dataset IDs to grab
+#' @param ids (numeric) Vector of BLE dataset IDs to grab
 #'
 #' @return (list) A nested list. 1st-level list item corresponds to dataset, 2nd-level items correspond to entities within a dataset and contain a string of the entity Id. List items are named accordingly (dataset after the packageId and entities after the entity name).
 #'
-#' @examples
 get_uncollated_entities <- function(ids = NULL) {
   # list of BLE Core Program ids. do we need to update them when new ids come out? seems not optimal and NOT good practice
   cp <-
@@ -43,13 +42,13 @@ get_uncollated_entities <- function(ids = NULL) {
   # construct pkgs ids
   pkg_ids <- paste0("knb-lter-ble.", ids, ".", rev)
 
-  message("Asking EDI for newest data...")
+  message("Querying Environmental Data Initiative (EDI) for the latest BLE data versions...")
 
   # loop over pkg ids and ask for ALL entity IDs and names from each pkg
   allids <- lapply(pkg_ids, EDIutils::read_data_entity_names)
 
   # filter elist by Ids supplied
-  elist <- elist[which(ids %in% cp)]
+  elist <- elist[match(ids, cp)]
 
   # get just the entity IDs we want
   eids <- sapply(seq_along(allids), function(x) {
@@ -83,18 +82,18 @@ get_uncollated_entities <- function(ids = NULL) {
 
 #' Download BLE data files slated for collation
 #'
-#' @param ids (numeric) Vector of dataset IDs to grab, default is to get all data
+#' @param ids (numeric) Vector of BLE dataset IDs to grab, default is to get all data
 #' @param path (character) Path to write CSVs to, defaults to working directory
-#' @param write (boolean) Whether to write data entities to file, defaults to FALSE
-#' @param metadata (boolean) whether to get metadata, defaults to TRUE
+#' @param write (logical) Whether to write data entities to file, defaults to FALSE
+#' @param skip_metadata (logical) whether to skip metadata, defaults to FALSE
 #'
-#' @return (list) A nested list of data frames. 1st-level list items are named "data" and "metadata" (if metadata = TRUE). For "data": 2nd-level list items correspond to datasets, 3rd-level items correspond to entities within a dataset and contain a data.frame of the data entity. List items are named accordingly (dataset after the packageId and entities after the entity name). Optionally, if write=T, functions also write to CSVs files in specified path. File names are the full package Id, followed by two underscores, followed by the full entity name. For "metadata": 2nd-level list items correspond to datasets, and contain a the metadata for that dataset in "emld" aka list format.
+#' @return (list) A nested list of data frames. 1st-level list items are named "data" and "metadata" (if metadata = TRUE). For "data": 2nd-level list items correspond to datasets, 3rd-level items correspond to entities within a dataset and contain a data.frame of the data entity. List items are named accordingly (dataset after the packageId and entities after the entity name). Optionally, if write=T, functions also write to CSVs files in specified path. File names are the full package Id, followed by two underscores, followed by the full entity name. For "metadata": 2nd-level list items correspond to datasets, and contain the metadata for that dataset in "emld" aka list format.
 #'
 #' @examples
 get_uncollated_dfs <- function(ids = NULL,
                           path = NULL,
                           write = FALSE,
-                          metadata = TRUE) {
+                          skip_metadata = FALSE) {
 
   if (is.null(path))
     path <- getwd()
@@ -102,7 +101,7 @@ get_uncollated_dfs <- function(ids = NULL,
   # checking if path exists
   stopifnot(dir.exists(path))
   e <- get_uncollated_entities(ids)
-  message("Downloading data files...")
+  message("Downloading latest data files...")
 
   # if you read this bit of code
   # beware that all the "raw"s are different
@@ -162,14 +161,14 @@ get_uncollated_dfs <- function(ids = NULL,
   output = list(data = dfs)
 
   # return metadata
-  if (metadata) {
+  if (!skip_metadata) {
     m <- lapply(names(e), FUN = EDIutils::read_metadata)
     names(m) <- names(e)
     m <- lapply(m, emld::as_emld)
     output$metadata <- m
   }
 
-  message("Done.")
+  message("Download complete!")
   return(output)
 }
 
@@ -199,4 +198,67 @@ preprocess <- function(df, ysi = F) {
   # in case any datasets still have "STL" instead of "SSL" for Stefasson Sound
   df$station <- gsub("STL", "SSL", df$station)
   return(df)
+}
+
+#' Reformat DOI to a standard format
+#'
+#' @param identifier (character) DOI string
+#'
+#' @return (character) DOI string in standard format, e.g. 10.1029/2020gb006552. Without URL heads like "https://doi.org" or the "doi:" prefix.
+#' @export
+clean_identifier <- function(identifier) {
+  identifier <- trimws(sub(" ", "", identifier))
+
+  if (any(grepl("https://doi.org", identifier))) {
+    identifier <- sub("https://doi.org/", "", identifier)
+  }
+  if (any(grepl("doi:", identifier))) {
+    identifier <- sub("doi:", "", identifier)
+  }
+  identifier <- trimws(sub(" ", "", identifier))
+  return(identifier)
+}
+
+
+
+#' @param x (list) attribute EML node
+#'
+#' @return
+#'
+#' @examples
+parse_attribute <- function(x) {
+  ## get full attribute list
+  att <- unlist(x, recursive = TRUE, use.names = TRUE)
+  measurementScale <- names(x$measurementScale)
+  domain <- names(x$measurementScale[[measurementScale]])
+
+  if (length(domain) == 1) {
+    ## domain == "nonNumericDomain"
+    domain <-
+      names(x$measurementScale[[measurementScale]][[domain]])
+  }
+  domain <- domain[grepl("Domain", domain)]
+
+  if (measurementScale == "dateTime" & is.null(domain)) {
+    domain <- "dateTimeDomain"
+  }
+
+  att <-
+    c(att, measurementScale = measurementScale, domain = domain)
+
+  ## separate factors
+  att <- att[!grepl("enumeratedDomain", names(att))]
+
+  ## separate methods
+  att <- att[!grepl("methods", names(att))]
+
+  ## Alter names to be consistent with other tools
+  names(att) <- gsub("standardUnit|customUnit",
+                     "unit",
+                     names(att))
+  names(att) <- gsub(".+\\.+",
+                     "",
+                     names(att))
+  att <- as.data.frame(t(att), stringsAsFactors = FALSE)
+  return(att)
 }
